bd882b5ea4e0b7636652feef1be7b3d60730b754
Simon Mitchell <simon@3sidedcube.com>
2018-02-22 16:26:06 +0000

    re-indents TableSection.swift

--- ThunderTable/TableSection.swift
+++ ThunderTable/TableSection.swift
@@ -19,49 +19,49 @@ public protocol Section {
     var header: String? { get set }
     
     var footer: String? { get set }
-	
-	var editHandler: EditHandler? { get set }
+    
+    var editHandler: EditHandler? { get set }
     
     var selectionHandler: SelectionHandler? { get set }
-	
-	var rowLeadingSwipeActionsConfiguration: SwipeActionsConfigurable? { get }
-	
-	var rowTrailingSwipeActionsConfiguration: SwipeActionsConfigurable? { get }
+    
+    var rowLeadingSwipeActionsConfiguration: SwipeActionsConfigurable? { get }
+    
+    var rowTrailingSwipeActionsConfiguration: SwipeActionsConfigurable? { get }
 }
 
 public extension Section {
-	
-	var rows: [Row] {
-		get {
-			return []
-		}
-		set {}
-	}
-	
+    
+    var rows: [Row] {
+        get {
+            return []
+        }
+        set {}
+    }
+    
     var header: String? {
-		get {
-			return nil
-		}
-		set {}
+        get {
+            return nil
+        }
+        set {}
     }
     
     var footer: String? {
-		get {
-			return nil
-		}
-		set {}
+        get {
+            return nil
+        }
+        set {}
     }
-
+    
     var selectionHandler: SelectionHandler? {
-		get {
-			return nil
-		}
-		set {}
+        get {
+            return nil
+        }
+        set {}
     }
-	
-	var rowLeadingSwipeActionsConfiguration: SwipeActionsConfigurable? { get { return nil } }
-	
-	var rowTrailingSwipeActionsConfiguration: SwipeActionsConfigurable? { get { return nil } }
+    
+    var rowLeadingSwipeActionsConfiguration: SwipeActionsConfigurable? { get { return nil } }
+    
+    var rowTrailingSwipeActionsConfiguration: SwipeActionsConfigurable? { get { return nil } }
 }
 
 open class TableSection: Section {
@@ -73,12 +73,12 @@ open class TableSection: Section {
     open var rows: [Row]
     
     open var selectionHandler: SelectionHandler?
-	
-	open var editHandler: EditHandler?
-	
-	open var rowLeadingSwipeActionsConfiguration: SwipeActionsConfigurable?
-	
-	open var rowTrailingSwipeActionsConfiguration: SwipeActionsConfigurable?
+    
+    open var editHandler: EditHandler?
+    
+    open var rowLeadingSwipeActionsConfiguration: SwipeActionsConfigurable?
+    
+    open var rowTrailingSwipeActionsConfiguration: SwipeActionsConfigurable?
     
     public init(rows: [Row], header: String? = nil, footer: String? = nil, selectionHandler: SelectionHandler? = nil) {
         
@@ -87,41 +87,41 @@ open class TableSection: Section {
         self.footer = footer
         self.selectionHandler = selectionHandler
     }
-	
-	/// Returns an array of `TableSection` objects sorted by first letter of the row's title
-	///
-	/// - Parameters:
-	///   - rows: The rows to sort into alphabetised sections
-	///   - selectionHandler: A selection handler to add to the sections
-	/// - Returns: An array of `TableSection` objects
-	public class func sortedSections(with rows: [Row], selectionHandler: SelectionHandler? = nil) -> [TableSection] {
-		
-		let sortedAlphabetically = self.alphabeticallySort(rows: rows)
-		let sortedKeys = sortedAlphabetically.keys.sorted { (stringA, stringB) -> Bool in
-			return stringB > stringA
-		}
-			
-		return sortedKeys.flatMap({key -> TableSection? in
-			guard let rows = sortedAlphabetically[key] else { return nil }
-			return TableSection(rows: rows, header: key, footer: nil, selectionHandler: selectionHandler)
-		})
-	}
-	
-	private class func alphabeticallySort(rows: [Row]) -> [String : [Row]] {
-		
-		var sortedDict = [String : [Row]]()
-		
-		rows.forEach { (row) in
-			
-			var firstLetter = "?"
-			if let rowTitle = row.title, !rowTitle.isEmpty {
-				firstLetter = String(rowTitle.prefix(1)).uppercased()
-			}
-			var subItems = sortedDict[firstLetter] ?? []
-			subItems.append(row)
-			sortedDict[firstLetter] = subItems
-		}
-		
-		return sortedDict
-	}
+    
+    /// Returns an array of `TableSection` objects sorted by first letter of the row's title
+    ///
+    /// - Parameters:
+    ///   - rows: The rows to sort into alphabetised sections
+    ///   - selectionHandler: A selection handler to add to the sections
+    /// - Returns: An array of `TableSection` objects
+    public class func sortedSections(with rows: [Row], selectionHandler: SelectionHandler? = nil) -> [TableSection] {
+        
+        let sortedAlphabetically = self.alphabeticallySort(rows: rows)
+        let sortedKeys = sortedAlphabetically.keys.sorted { (stringA, stringB) -> Bool in
+            return stringB > stringA
+        }
+        
+        return sortedKeys.flatMap({key -> TableSection? in
+            guard let rows = sortedAlphabetically[key] else { return nil }
+            return TableSection(rows: rows, header: key, footer: nil, selectionHandler: selectionHandler)
+        })
+    }
+    
+    private class func alphabeticallySort(rows: [Row]) -> [String : [Row]] {
+        
+        var sortedDict = [String : [Row]]()
+        
+        rows.forEach { (row) in
+            
+            var firstLetter = "?"
+            if let rowTitle = row.title, !rowTitle.isEmpty {
+                firstLetter = String(rowTitle.prefix(1)).uppercased()
+            }
+            var subItems = sortedDict[firstLetter] ?? []
+            subItems.append(row)
+            sortedDict[firstLetter] = subItems
+        }
+        
+        return sortedDict
+    }
 }
